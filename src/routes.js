9570ed5ccf2c1ee9c5fdc49c0c64f320f9f33225
VuThiBichThu <vuthibichthu.it@gmail.com>
2021-04-15 11:16:40 +0700

    WIP - making group permissions page

--- src/routes.js
+++ src/routes.js
@@ -6,27 +6,25 @@ import Moderator from "./components/usersmanagement/moderators/Moderator";
 import Companies from "./components/usersmanagement/companies/Companies";
 import ITers from "./components/usersmanagement/ITers/ITers";
 
-import Users from "./components/grouppermissions/Users";
-import User from "./components/grouppermissions/User";
-
 import PostsApproval from "./components/postsmanagement/posts-approval/PostsApproval";
 import PostsStatistic from "./components/postsmanagement/posts-statistic/PostsStatistic";
+
+import GroupPermissions from "./containers/admin/GroupPermissions"
 
 const routes = [
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
 
-  { path: '/usersmanagement', name: 'Users Management', component: Users, exact: true },
+  { path: '/usersmanagement', name: 'Users Management', component: GroupPermissions, exact: true },
   { path: '/usersmanagement/moderators', name: 'Moderators', component: Moderators, exact: true},
   { path: '/usersmanagement/moderators/:id/:name', name: 'Moderator Permissions', component: Moderator, exact: true},
   { path: '/usersmanagement/companies', name: 'Companies', component: Companies  },
   { path: '/usersmanagement/ITers', name: 'ITers', component: ITers },
 
-  { path: '/postsmanagement', name: 'Posts Management', component: Users, exact: true },
+  { path: '/postsmanagement', name: 'Posts Management', component: PostsApproval, exact: true },
   { path: '/postsmanagement/posts-approval', name: 'Posts Approval', component: PostsApproval },
   { path: '/postsmanagement/posts-statistic', name: 'Posts Statistic', component: PostsStatistic },
 
-  { path: '/grouppermissions', exact: true, name: 'Group permissions', component: Users },
-  { path: '/grouppermissions/:id', exact: true, name: 'Group permissions details', component: User },
+  { path: '/grouppermissions', exact: true, name: 'Group permissions', component: GroupPermissions },
 
 ];
 
